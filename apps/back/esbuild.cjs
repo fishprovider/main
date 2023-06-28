990528299cf693be5fc00fd8a3f5f9f096be921d
Marco Dinh <maidh91@gmail.com>
2023-06-28 19:14:07 +1000

    back dev

--- apps/back/esbuild.cjs
+++ apps/back/esbuild.cjs
@@ -2,47 +2,48 @@ const esbuild = require('esbuild');
 const pkg = require('./package.json');
 const { globSync } = require('glob');
 
-const buildApi = () => {
-  const routeFiles = globSync('src/api/**/*.ts', { ignore: '**/*.test.*' });
+const watchMode = process.env.WATCH_MODE;
 
-  const options = {
-    entryPoints: routeFiles,
-    outdir: 'dist/api',
-    loader: { '.js': 'jsx', '.ts': 'tsx' },
-    external: Object.keys(pkg.dependencies),
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    splitting: true,
-    target: ['esnext'],
-    platform: 'node',
-    format: 'esm',
-  };
-
-  return esbuild.build(options);
+const apiOptions = {
+  entryPoints: globSync('src/api/**/*.ts', { ignore: '**/*.test.*' }),
+  outdir: 'dist/api',
+  loader: { '.js': 'jsx', '.ts': 'tsx' },
+  external: Object.keys(pkg.dependencies),
+  bundle: true,
+  minify: !watchMode,
+  sourcemap: !watchMode,
+  splitting: true,
+  target: ['esnext'],
+  platform: 'node',
+  format: 'esm',
 };
 
-const buildMain = () => {
-  const options = {
-    entryPoints: ['index.ts'],
-    outdir: 'dist',
-    loader: { '.js': 'jsx', '.ts': 'tsx' },
-    external: Object.keys(pkg.dependencies),
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    splitting: true,
-    target: ['esnext'],
-    platform: 'node',
-    format: 'esm',
-  };
-
-  return esbuild.build(options);
+const mainOptions = {
+  entryPoints: ['index.ts'],
+  outdir: 'dist',
+  loader: { '.js': 'jsx', '.ts': 'tsx' },
+  external: Object.keys(pkg.dependencies),
+  bundle: true,
+  minify: !watchMode,
+  sourcemap: !watchMode,
+  splitting: true,
+  target: ['esnext'],
+  platform: 'node',
+  format: 'esm',
 };
 
-const build = async () => {
-  await buildApi();
-  await buildMain();
+const main = async () => {
+  if (watchMode) {
+    const apiCtx = await esbuild.context(apiOptions);
+    const mainCtx = await esbuild.context(mainOptions);
+    await Promise.all([
+      apiCtx.watch(),
+      mainCtx.watch(),
+    ]);
+  } else {
+    await esbuild.build(apiOptions);
+    await esbuild.build(mainOptions);
+  }
 };
 
-build();
+main();
--- apps/back/index.ts
+++ apps/back/index.ts
@@ -1,7 +1,14 @@
+import { ProviderType } from '@fishbot/utils/constants/account';
+import delay from '@fishbot/utils/helpers/delay';
+import { getMajorPairs } from '@fishbot/utils/helpers/price';
 import { config } from 'dotenv-flow';
 
-const start = () => {
+const start = async () => {
   config();
-  import('./controllers/main').then((mod) => mod.start());
+
+  const majorPairs = getMajorPairs(ProviderType.exness);
+  console.log(majorPairs);
+  await delay(1000);
+  console.log('done');
 };
 start();
