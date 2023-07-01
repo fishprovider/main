7638c24cbf363ffbe550eab504b8ac3c9f6fb1ae
Marco Dinh <maidh91@gmail.com>
2023-07-01 22:00:48 +1000

    fix lint

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -1,8 +1,10 @@
+/* eslint-disable max-len */
+
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
