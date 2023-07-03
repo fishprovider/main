70dbfdfe75dbc18086013823e1374ad5fcba5038
Marco Dinh <maidh91@gmail.com>
2023-07-03 14:03:02 +1000

    fix-release

--- apps/web/vercel-build.sh
+++ apps/web/vercel-build.sh
@@ -4,9 +4,8 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "canary" || "$VERCEL_GIT_COMMIT_REF" == "release" ]]; then
   echo "✅ - Build needed"
+  exit 1
 else
   echo "🛑 - Build skipped"
   exit 0
 fi
-
-git diff --quiet HEAD^ HEAD .
