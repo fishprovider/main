9d1411c44ecfe2503a7f4312dd421f8029e7fcfb
Marco Dinh <maidh91@gmail.com>
2023-07-04 18:14:21 +1000

    fix-doctor

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 // https://github.com/vercel/next.js/blob/canary/packages/eslint-config-next/index.js
 // https://github.com/vercel/next.js/blob/canary/packages/eslint-plugin-next/lib/index.js
 
+// @ts-ignore no need to include in tsconfig
 const config = require('../../.eslintrc.cjs');
 
 module.exports = {
