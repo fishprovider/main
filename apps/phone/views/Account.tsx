e70bcfdac5e6d847c811d2e01aebe973ef24c0d1
Marco Dinh <maidh91@gmail.com>
2023-07-06 08:19:47 +1000

    fishbot/utils

--- apps/phone/views/Account.tsx
+++ apps/phone/views/Account.tsx
@@ -1,5 +1,6 @@
+import { ErrorType } from '@fishbot/utils/constants/error';
 import { StyleSheet } from 'react-native';
-import { Button } from 'tamagui';
+import { Button, Label } from 'tamagui';
 
 import EditScreenInfo from '~components/EditScreenInfo';
 import { Text, View } from '~components/Themed';
@@ -28,6 +29,7 @@ export default function Account() {
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/account.tsx" />
       <Button themeInverse>Hello</Button>
+      <Label>{ErrorType.accountNotFound}</Label>
     </View>
   );
 }
