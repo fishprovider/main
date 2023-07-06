58512ada7c3a7335526ef544d8453f26c470b325
Marco Dinh <maidh91@gmail.com>
2023-07-06 15:57:10 +1000

    remove icon

--- apps/phone/layouts/MainTabLayout.tsx
+++ apps/phone/layouts/MainTabLayout.tsx
@@ -64,7 +64,6 @@ export default function BaseLayout() {
         options={{
           title: 'Invest',
           tabBarIcon: InvestTabIcon,
-          headerRight: HeaderRight,
         }}
       />
       <Tabs.Screen
