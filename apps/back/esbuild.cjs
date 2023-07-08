6aabc1d8e3d748e3a976bd874eb00c78052726d7
Marco Dinh <maidh91@gmail.com>
2023-07-09 07:35:21 +1000

    clean esbuild back

--- apps/back/esbuild.cjs
+++ apps/back/esbuild.cjs
@@ -1,57 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const esbuild = require('esbuild');
 const { globSync } = require('glob');
+const build = require('../../esbuild.cjs');
+const dependencies = require('./package.json').dependencies;
 
 const watchMode = process.env.WATCH_MODE;
-const devMode = watchMode;
-
-const dependencies = {
-  ...require('./package.json').dependencies,
-  ...require('../../packages/utils/package.json').dependencies,
-  ...require('../../packages/ctrader/package.json').dependencies,
-  ...require('../../packages/metatrader/package.json').dependencies,
-  ...require('../../packages/swap/package.json').dependencies,
-  ...require('../../packages/coin/package.json').dependencies,
-  ...require('../../packages/core/package.json').dependencies,
-};
-
-const options = {
-  loader: { '.js': 'js', '.ts': 'ts' },
-  external: Object.keys(dependencies),
-  bundle: true,
-  minify: !devMode,
-  sourcemap: !devMode,
-  target: ['esnext'],
-  platform: 'node',
-  format: 'esm',
-};
 
 const apiOptions = {
-  ...options,
   entryPoints: globSync('api/**/*.ts', { ignore: '**/*.test.*' }),
   outdir: 'dist/api',
   splitting: false,
 };
 
-const mainOptions = {
-  ...options,
-  entryPoints: ['index.ts'],
-  outdir: 'dist',
-  splitting: true,
-};
-
 const main = async () => {
   if (watchMode) {
-    const apiCtx = await esbuild.context(apiOptions);
-    const mainCtx = await esbuild.context(mainOptions);
     await Promise.all([
-      apiCtx.watch(),
-      mainCtx.watch(),
+      await build(dependencies, apiOptions),
+      await build(dependencies)
     ]);
   } else {
-    await esbuild.build(apiOptions);
-    await esbuild.build(mainOptions);
+    await build(dependencies, apiOptions);
+    await build(dependencies);
   }
 };
 
