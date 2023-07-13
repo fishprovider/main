3fadd0fad028dd23183ceaa7056f97c131f0ba38
Marco Dinh <maidh91@gmail.com>
2023-07-13 14:33:31 +1000

    install drawer

--- apps/phone/views/Wallet.tsx
+++ apps/phone/views/Wallet.tsx
@@ -1,10 +1,44 @@
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { useNavigation } from '@react-navigation/native';
+// import { Button, View } from 'react-native';
+
 import PushNotif from '~components/PushNotif';
 import UserController from '~controllers/UserController';
+
+// const Drawer = createDrawerNavigator();
+
+// function HomeScreen() {
+//   const navigation = useNavigation<any>();
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Button
+//         onPress={() => navigation.toggleDrawer()}
+//         title="Toggle Drawer"
+//       />
+//     </View>
+//   );
+// }
+
+// function NotificationsScreen() {
+//   const navigation = useNavigation<any>();
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Button
+//         onPress={() => navigation.toggleDrawer()}
+//         title="Toggle Drawer"
+//       />
+//     </View>
+//   );
+// }
 
 export default function Wallet() {
   return (
     <UserController>
       <PushNotif />
+      {/* <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator> */}
     </UserController>
   );
 }
