6774fe2a348653e0956f7e09709b8b4d2d828eb8
Marco Dinh <maidh91@gmail.com>
2023-07-15 16:32:13 +1000

    fix build scripts

--- apps/back/build-packages.sh
+++ apps/back/build-packages.sh
@@ -3,15 +3,12 @@
 cd ../..
 npm run build -w packages/utils
 
-function build_swap {
-  npm run build -w packages/ctrader &
-  npm run build -w packages/metatrader &
-  # npm run build -w packages/binance &
-  wait
-  npm run build -w packages/swap
-}
-
+npm run build -w packages/ctrader &
+npm run build -w packages/metatrader &
+# npm run build -w packages/binance &
 npm run build -w packages/core &
+wait
+
+npm run build -w packages/swap &
 npm run build -w packages/coin &
-build_swap &
 wait
--- workers/spot-meta/build-packages.sh
+++ workers/spot-meta/build-packages.sh
@@ -3,14 +3,10 @@
 cd ../..
 npm run build -w packages/utils
 
-function build_swap {
-  npm run build -w packages/ctrader &
-  npm run build -w packages/metatrader &
-  # npm run build -w packages/binance &
-  wait
-  npm run build -w packages/swap
-}
-
+npm run build -w packages/ctrader &
+npm run build -w packages/metatrader &
+# npm run build -w packages/binance &
 npm run build -w packages/core &
-build_swap &
 wait
+
+npm run build -w packages/swap
