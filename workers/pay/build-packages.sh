58ce9746b4210a603a99f27422c3ef78a8688b92
Marco Dinh <maidh91@gmail.com>
2023-07-15 20:18:54 +1000

    fix build

--- workers/pay/build-packages.sh
+++ workers/pay/build-packages.sh
@@ -2,11 +2,5 @@
 
 cd ../..
 npm run build -w packages/utils
-
-npm run build -w packages/ctrader &
-npm run build -w packages/metatrader &
-# npm run build -w packages/binance &
-npm run build -w packages/core &
-wait
-
-npm run build -w packages/swap
+npm run build -w packages/core
+npm run build -w packages/coin
