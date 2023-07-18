0c43b2ebfcf2a67ee852f55d2b3776e2a78777e9
Marco Dinh <maidh91@gmail.com>
2023-07-18 18:42:33 +1000

    update scripts

--- apps/back/build-packages.sh
+++ apps/back/build-packages.sh
@@ -16,15 +16,28 @@ function old() {
   wait
 }
 
+#
+# Clean Architecture
+#
+
+function adapters() {
+  npm run build -w adapters/backend &
+  # npm run build -w adapters/frontend &
+  wait
+}
+
+function frameworks() {
+  npm run build -w frameworks/mongo &
+  npm run build -w frameworks/redis &
+  wait
+}
+
 function new() {
   npm run build -w packages/enterprise-rules
   npm run build -w packages/application-rules
 
-  npm run build -w adapters/backend &
-  # npm run build -w adapters/frontend &
-
-  npm run build -w frameworks/mongo &
-  npm run build -w frameworks/redis &
+  adapters &
+  frameworks &
   wait
 }
 
