b5979143e9c6297dfc8ec6310e4a32b90ec3377a
Marco Dinh <maidh91@gmail.com>
2023-07-20 10:12:51 +1000

    cacheFirst

--- apps/back/controllers/main.ts
+++ apps/back/controllers/main.ts
@@ -1,6 +1,5 @@
 import { destroyAsync, start as startCore } from '@fishprovider/core/dist/controllers/main';
-import { mongo } from '@fishprovider/framework-mongo';
-import { redis } from '@fishprovider/framework-redis';
+import { cacheFirst } from '@fishprovider/framework-cache-first';
 
 import * as adapter from '~controllers/adapter';
 
@@ -9,18 +8,12 @@ const start = async () => {
   await adapter.start();
 
   // Clean architecture
-  await Promise.all([
-    redis.start(),
-    mongo.start(),
-  ]);
+  await cacheFirst.start();
 };
 
 const destroy = async () => {
   // Clean architecture
-  await Promise.all([
-    redis.stop(),
-    mongo.stop(),
-  ]);
+  await cacheFirst.stop();
 
   await destroyAsync(adapter);
 };
