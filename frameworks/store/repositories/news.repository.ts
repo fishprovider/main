6d62a7b0a54ca63ddff8035a10a2e1ff6f62aaae
Marco Dinh <maidh91@gmail.com>
2023-07-21 01:01:05 +1000

    useSelector

--- frameworks/store/repositories/news.repository.ts
+++ frameworks/store/repositories/news.repository.ts
@@ -8,9 +8,9 @@ async function setNews(params: SetNewsRepositoryParams) {
 }
 
 async function getNews<T>(params: GetNewsRepositoryParams<T>) {
-  const { selector } = params;
-  if (selector) {
-    return storeNews.useStore<T>(selector);
+  const { useSelector } = params;
+  if (useSelector) {
+    return storeNews.useStore<T>(useSelector);
   }
   return Object.values(storeNews.getState());
 }
