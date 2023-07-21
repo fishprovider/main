b555354a3dd61bbcafebb47f7bd2e7c938010d5e
Marco Dinh <maidh91@gmail.com>
2023-07-21 14:29:10 +1000

    StoreNewsRepository.setNews

--- frameworks/offline-first/repositories/news.repository.ts
+++ frameworks/offline-first/repositories/news.repository.ts
@@ -1,6 +1,7 @@
 import { DefaultNewsRepository, type GetNewsRepositoryParams, type NewsRepository } from '@fishprovider/application-rules';
 import { FishApiNewsRepository } from '@fishprovider/framework-fish-api';
 import { buildSetNewsKeys, LocalNewsRepository } from '@fishprovider/framework-local';
+import { StoreNewsRepository } from '@fishprovider/framework-store';
 
 async function getNews(params: GetNewsRepositoryParams) {
   let news = await LocalNewsRepository.getNews(params);
@@ -10,6 +11,7 @@ async function getNews(params: GetNewsRepositoryParams) {
     // non-blocking
     LocalNewsRepository.setNews({ keys, news: news || [] });
   } else {
+    StoreNewsRepository.setNews({ news });
     // non-blocking
     FishApiNewsRepository.getNews(params).then((res) => {
       LocalNewsRepository.setNews({ keys, news: res || [] });
