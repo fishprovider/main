01cbf88a71cf779f0d55b90247d55bdcf4e85fa2
Marco Dinh <maidh91@gmail.com>
2023-07-24 21:58:45 +1000

    clean BaseController

--- apps/phone/App.tsx
+++ apps/phone/App.tsx
@@ -1,10 +1,5 @@
-import BaseController from '~controllers/BaseController';
 import RootNavigator from '~navigators/RootNavigator';
 
 export default function App() {
-  return (
-    <BaseController>
-      <RootNavigator />
-    </BaseController>
-  );
+  return <RootNavigator />;
 }
--- apps/phone/navigators/RootNavigator.tsx
+++ apps/phone/navigators/RootNavigator.tsx
@@ -3,6 +3,7 @@
 import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import BaseController from '~controllers/BaseController';
 import Strategies from '~views/Strategies';
 
 import AccountNavigator from './AccountNavigator';
@@ -12,30 +13,32 @@ const Tab = createBottomTabNavigator();
 
 export default function RootNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Strategies"
-        component={Strategies}
-        options={{
-          tabBarIcon: (props) => <FontAwesome {...props} name="rocket" />,
-        }}
-      />
-      <Tab.Screen
-        name="Wallet"
-        component={WalletNavigator}
-        options={{
-          tabBarIcon: (props) => <FontAwesome {...props} name="bank" />,
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Account"
-        component={AccountNavigator}
-        options={{
-          tabBarIcon: (props) => <FontAwesome {...props} name="bar-chart" />,
-          headerShown: false,
-        }}
-      />
-    </Tab.Navigator>
+    <BaseController>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Strategies"
+          component={Strategies}
+          options={{
+            tabBarIcon: (props) => <FontAwesome {...props} name="rocket" />,
+          }}
+        />
+        <Tab.Screen
+          name="Wallet"
+          component={WalletNavigator}
+          options={{
+            tabBarIcon: (props) => <FontAwesome {...props} name="bank" />,
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={AccountNavigator}
+          options={{
+            tabBarIcon: (props) => <FontAwesome {...props} name="bar-chart" />,
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
+    </BaseController>
   );
 }
