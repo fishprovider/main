20a69d1722e4298c26b7bf8dfef7e12ef63def4e
Marco Dinh <maidh91@gmail.com>
2023-07-25 14:50:07 +1000

    clean hooks

--- apps/phone/controllers/BaseController/UserNotif.tsx
+++ apps/phone/controllers/BaseController/UserNotif.tsx
@@ -1,7 +1,20 @@
-import useUserNotif from '~hooks/useUserNotif';
+import { useEffect } from 'react';
+
+import { handleNotif, subNotif } from '~libs/pushNotif';
 
 function UserNotif() {
-  useUserNotif();
+  useEffect(() => {
+    subNotif();
+
+    let unsub: () => void;
+    handleNotif().then((res) => {
+      unsub = res;
+    });
+    return () => {
+      if (unsub) unsub();
+    };
+  }, []);
+
   return null;
 }
 
