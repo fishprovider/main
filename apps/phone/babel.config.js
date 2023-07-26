655fd05f36dd144be838ac2191d68a50306f9cef
Marco Dinh <maidh91@gmail.com>
2023-07-26 19:23:13 +1000

    add env

--- apps/phone/babel.config.js
+++ apps/phone/babel.config.js
@@ -1,4 +1,8 @@
 process.env.TAMAGUI_TARGET = 'native';
+process.env.EXPO_PUBLIC_BACKEND_URL = 'https://back.fishprovider.com';
+process.env.EXPO_PUBLIC_SOCKET_URL = 'wss://back.fishprovider.com';
+process.env.EXPO_PUBLIC_DEMO_BACKEND_URL = 'https://back-demo.fishprovider.com';
+process.env.EXPO_PUBLIC_DEMO_SOCKET_URL = 'wss://back-demo.fishprovider.com';
 
 // eslint-disable-next-line func-names
 module.exports = function (api) {
@@ -11,6 +15,10 @@ module.exports = function (api) {
         {
           include: [
             'TAMAGUI_TARGET',
+            'EXPO_PUBLIC_BACKEND_URL',
+            'EXPO_PUBLIC_SOCKET_URL',
+            'EXPO_PUBLIC_DEMO_BACKEND_URL',
+            'EXPO_PUBLIC_DEMO_SOCKET_URL',
           ],
         },
       ],
