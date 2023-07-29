a9478175977917a03590249fb195990ba2941819
Marco Dinh <maidh91@gmail.com>
2023-07-29 17:18:43 +1000

    fix script

--- scripts/clean-frontend-deps.sh
+++ scripts/clean-frontend-deps.sh
@@ -2,10 +2,8 @@
 
 APP=$1
 
-cd ..
-
-cd adapters && ls | grep -vE "frontend" | xargs rm -rf
-cd apps && ls | grep -vE "$APP" | xargs rm -rf
-cd frameworks && ls | grep -vE "local|store|fish-api|offline-first" | xargs rm -rf
-cd packages && ls | grep -vE "utils|cross|enterprise-rules|application-rules" | xargs rm -rf
-rm -rf workers
+cd ../adapters && ls | grep -vE "frontend" | xargs rm -rf
+cd ../apps && ls | grep -vE "$APP" | xargs rm -rf
+cd ../frameworks && ls | grep -vE "local|store|fish-api|offline-first" | xargs rm -rf
+cd ../packages && ls | grep -vE "utils|cross|enterprise-rules|application-rules" | xargs rm -rf
+cd ../workers && rm -rf *
