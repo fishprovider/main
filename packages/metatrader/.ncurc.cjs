4b1fa063cefdf3ff4b781734e3e341903092ee37
Marco Dinh <maidh91@gmail.com>
2023-08-06 06:26:18 +1000

    up deps

--- packages/metatrader/.ncurc.cjs
+++ packages/metatrader/.ncurc.cjs
@@ -2,9 +2,6 @@ module.exports = {
   color: true,
   format: ["group", "ownerChanged", "repo"],
   reject: [
-    '@types/long',
-    'eslint-config-next',
-    'glob',
-    'protobufjs',
+    'metaapi.cloud-sdk',
   ],
 };
