bc18c6c22351e597bd4094030c3a750cf52d2661
Marco Dinh <maidh91@gmail.com>
2023-08-09 17:35:53 +1000

    concurrency

--- apps/web/libs/notion.ts
+++ apps/web/libs/notion.ts
@@ -53,9 +53,6 @@ export const getDefaultStaticPaths = (
       rootId,
       undefined,
       getPage,
-      {
-        concurrency: 8,
-      },
     );
 
     const mapPageUrl = async (pageId: string) => {
