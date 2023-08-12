86f68fe14ab9124dc0043ce7755d66b5de874970
Marco Dinh <maidh91@gmail.com>
2023-08-12 11:03:25 +1000

    up firebasejs

--- apps/web/public/firebase-messaging-sw.js
+++ apps/web/public/firebase-messaging-sw.js
@@ -2,8 +2,8 @@
 
 /* eslint-disable no-undef */
 
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: 'AIzaSyDoQYfmz1H8tzjv-nMLln84oeqvdFDjVT4',
