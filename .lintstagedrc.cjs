a5c5e7a0d67cf7cd6711103f11f43cf38ca65e54
Marco Dinh <maidh91@gmail.com>
2023-08-14 13:17:45 +1000

    test-husky

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,14 @@
-module.exports = {
-  "apps/phone/**/*.{js,jsx,ts,tsx}": "npm run -w apps/phone ci-lint",
-  "apps/phone/**/*.{ts,tsx}": "npm run -w apps/phone ci-type-check",
-};
+const { globSync } = require('glob');
+const rootPackageJson = require('../package.json');
+
+const workSpaceDirs = rootPackageJson.workspaces.flatMap((item) => globSync(item));
+// console.log(workSpaceDirs);
+
+const config = {};
+workSpaceDirs.forEach(workSpaceDir => {
+  config[`${workSpaceDir}/**/*.{js,jsx,ts,tsx}`] = `npm run -w ${workSpaceDir} ci-lint`;
+  config[`${workSpaceDir}/**/*.{ts,tsx}`] = `npm run -w ${workSpaceDir} ci-type-check`;
+})
+// console.log(config);
+
+module.exports = config;
