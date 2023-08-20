7df7f1ba1b194f7c291673af753d682513170c40
Marco Dinh <maidh91@gmail.com>
2023-08-20 11:38:13 +1000

    fix-UserNotif

--- apps/phone/controllers/BaseController/UserNotif.tsx
+++ apps/phone/controllers/BaseController/UserNotif.tsx
@@ -1,19 +1,34 @@
+import storeUser from '@fishprovider/cross/dist/stores/user';
 import { useEffect } from 'react';
 
 import { handleNotif, subNotif } from '~libs/pushNotif';
 
 function UserNotif() {
-  useEffect(() => {
-    subNotif();
+  const {
+    isClientLoggedIn,
+    isServerLoggedIn,
+  } = storeUser.useStore((state) => ({
+    isClientLoggedIn: state.isClientLoggedIn,
+    isServerLoggedIn: state.isServerLoggedIn,
+  }));
 
+  useEffect(() => {
     let unsub: () => void;
-    handleNotif().then((res) => {
-      unsub = res;
-    });
+    if (isClientLoggedIn) {
+      handleNotif().then((res) => {
+        unsub = res;
+      });
+    }
     return () => {
       if (unsub) unsub();
     };
-  }, []);
+  }, [isClientLoggedIn]);
+
+  useEffect(() => {
+    if (isServerLoggedIn) {
+      subNotif();
+    }
+  }, [isServerLoggedIn]);
 
   return null;
 }
