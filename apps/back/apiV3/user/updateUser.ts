c7f4145294664f5a99fd377d2513040059a243ff
Marco Dinh <maidh91@gmail.com>
2023-08-29 18:28:39 +1000

    starProvider

--- apps/back/apiV3/user/updateUser.ts
+++ apps/back/apiV3/user/updateUser.ts
@@ -1,4 +1,4 @@
-import { AccountRoles, updateUserService, User } from '@fishprovider/core-new';
+import { updateUserService, User } from '@fishprovider/core-new';
 import { MongoUserRepository } from '@fishprovider/repository-mongo';
 import { z } from 'zod';
 
@@ -6,26 +6,9 @@ import { ApiHandler } from '~types/ApiHandler.model';
 
 const handler: ApiHandler<Partial<User>> = async (data, userSession) => {
   const params = z.object({
-    userId: z.string().optional(),
-    email: z.string().optional(),
-    name: z.string().optional(),
-    picture: z.string().optional(),
-    roles: z.object({
-      admin: z.boolean().optional(),
-      adminWeb: z.boolean().optional(),
-      managerWeb: z.boolean().optional(),
-      adminProviders: z.record(z.boolean()).optional(),
-      protectorProviders: z.record(z.boolean()).optional(),
-      traderProviders: z.record(z.boolean()).optional(),
-      viewerProviders: z.record(z.boolean()).optional(),
-    }).optional(),
     starProvider: z.object({
       accountId: z.string(),
       enabled: z.boolean(),
-    }).optional(),
-    addRole: z.object({
-      accountId: z.string(),
-      role: z.nativeEnum(AccountRoles),
     }).optional(),
   }).strict()
     .parse(data);
