d51564af4271bee7946dec68d131dd15968b01b1
Marco Dinh <maidh91@gmail.com>
2023-08-29 20:00:49 +1000

    fix-clean-deps

--- scripts/clean-frontend-deps.sh
+++ scripts/clean-frontend-deps.sh
@@ -2,9 +2,10 @@
 
 APP=$1
 
-cd ../packages && ls | grep -vE "utils|cross" | xargs rm -rf
+cd ../packages && ls | grep -vE "utils|cross|core-new" | xargs rm -rf
 cd ../workers && rm -rf *
 cd ../apps && ls | grep -vE "$APP" | xargs rm -rf
 
 cd ../adapters && ls | grep -vE "frontend" | xargs rm -rf
 cd ../frameworks-backend && rm -rf *
+cd ../repositories-backend && rm -rf *
