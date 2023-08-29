c492b329f7ac35ed20438fb37594425115f130e0
Marco Dinh <maidh91@gmail.com>
2023-08-29 20:55:39 +1000

    apiGet

--- packages/cross/api/user/getUser.ts
+++ packages/cross/api/user/getUser.ts
@@ -1,12 +1,12 @@
 import type { User } from '@fishprovider/utils/dist/types/User.model';
 
-import { apiPost } from '~libs/api';
+import { apiGet } from '~libs/api';
 import storeUser from '~stores/user';
 
 const getUser = async (payload: {
   reload?: boolean;
 }) => {
-  const updatedInfo = await apiPost<User>('/v3/user/getUser', payload);
+  const updatedInfo = await apiGet<User>('/v3/user/getUser', payload);
   const info = {
     ...storeUser.getState().info,
     ...updatedInfo,
