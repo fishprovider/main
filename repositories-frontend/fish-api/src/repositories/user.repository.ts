62ff11a732ec737d527800f9a2ab42350ed21153
Marco Dinh <maidh91@gmail.com>
2023-08-30 23:26:07 +1000

    fish api

---
+++ repositories-frontend/fish-api/src/repositories/user.repository.ts
@@ -0,0 +1,35 @@
+import {
+  BaseGetOptions, BaseUpdateOptions,
+  type GetUserFilter, type UpdateUserPayload, type User, type UserRepository,
+} from '@fishprovider/core-new';
+
+// import { StoreUserRepository } from '@fishprovider/framework-store';
+import { fishApi } from '../main';
+
+const getUser = async (
+  filter: GetUserFilter,
+  _options: BaseGetOptions<User>,
+) => {
+  const { apiGet } = await fishApi.get();
+  const user = await apiGet<Partial<User> | null | undefined>('/user/getUser', filter);
+  // StoreUserRepository.setUser({ user });
+  return { doc: user };
+};
+
+const updateUser = async (
+  filter: GetUserFilter,
+  payload: UpdateUserPayload,
+  options: BaseUpdateOptions<User>,
+) => {
+  const { apiPost } = await fishApi.get();
+  const user = await apiPost<Partial<User> | null | undefined>('/user/updateUser', {
+    filter, payload, options,
+  });
+  // StoreUserRepository.setUser({ user });
+  return { doc: user };
+};
+
+export const FishApiUserRepository: UserRepository = {
+  getUser,
+  updateUser,
+};
