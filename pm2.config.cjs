9029a61cc50d835e4cf6bf8ec10be1d93d993644
Marco Dinh <maidh91@gmail.com>
2023-09-07 16:05:37 +1000

    pm2-web

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,100 +1,143 @@
-const envBackend = {
-  HUSKY: 0,
-  DOPPLER_PROJECT: 'backend',
-  DOPPLER_CONFIG: 'prd_fish_server',
-};
+//
+// frontend
+//
 
-const appConfigs = [
+const frontendApps = [
   {
-    workspace: 'apps',
     name: 'web',
     env: {
+      HUSKY: 0,
+      DOPPLER_PROJECT: 'web',
+      DOPPLER_CONFIG: 'prd',
       PORT: 3000,
     },
+    script: 'npm',
+    args: 'run start -w apps/web',
   },
+];
+
+//
+// backend
+//
+
+const backendAppConfigBase = {
+  workspace: 'workers',
+  script: 'npm',
+  env: {
+    HUSKY: 0,
+    DOPPLER_PROJECT: 'backend',
+    DOPPLER_CONFIG: 'prd_fish_server',
+  },
+};
+
+const backendAppConfigs = [
   {
+    ...backendAppConfigBase,
     workspace: 'apps',
     name: 'back',
     cron_restart: '5 1 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 3001,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'bot',
     cron_restart: '15 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8007,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'copy',
     cron_restart: '20 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8006,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'cron',
     cron_restart: '5 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8002,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'gate',
     cron_restart: '0 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8001,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'head-ctrader',
     cron_restart: '25 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8005,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'head-meta',
     cron_restart: '30 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8009,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'mon',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8000,
       DRY_RUN: true,
     },
     cron_restart: '50 0 * * *',
   },
   {
+    ...backendAppConfigBase,
     name: 'pay',
     cron_restart: '10 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8020,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'pup',
     cron_restart: '45 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8003,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'spot-ctrader',
     cron_restart: '35 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8004,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'spot-ctrader-poll',
     project: 'spot-ctrader',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8104,
       TYPE_ID: 'spot-ctrader-icmarkets-poll',
       SPOT_TASKS: 'poll',
@@ -103,16 +146,20 @@ const appConfigs = [
     cron_restart: '55 0 * * *',
   },
   {
+    ...backendAppConfigBase,
     name: 'spot-meta',
     cron_restart: '40 0 * * *',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8008,
     },
   },
   {
+    ...backendAppConfigBase,
     name: 'spot-meta-poll',
     project: 'spot-meta',
     env: {
+      ...backendAppConfigBase.env,
       PORT: 8108,
       TYPE_ID: 'spot-meta-exness-poll',
       SPOT_TASKS: 'poll',
@@ -122,24 +169,27 @@ const appConfigs = [
   },
 ];
 
-const apps = appConfigs.map(({
-  workspace = 'workers',
-  name,
-  project,
-  env,
-  cron_restart,
-}) => ({
-  name,
-  env: {
-    ...envBackend,
-    ...env,
-  },
-  cron_restart,
-  script: 'npm',
-  args: `run start -w ${workspace}/${project || name}`,
-}));
+const backendApps = backendAppConfigs.map((config) => {
+  const {
+    workspace,
+    name,
+    project = name,
+    args = `run start -w ${workspace}/${project}`,
+  } = config;
+  return {
+    ...config,
+    args,
+  };
+});
 
-const deployConfig = {
+const apps = [...backendApps, ...frontendApps];
+
+//
+// deploy
+//
+
+const deployConfigBase = {
+  user: 'marco',
   repo: 'git@gitlab.com:fishprovider/main.git',
   ref: 'origin/master',
   'post-deploy': 'git rev-parse HEAD',
@@ -147,14 +197,12 @@ const deployConfig = {
 
 const deploy = {
   localhost: {
-    ...deployConfig,
-    user: 'marco',
+    ...deployConfigBase,
     host: 'localhost',
     path: '/Users/marco/pm2-apps/fishprovider',
   },
   fish: {
-    ...deployConfig,
-    user: 'marco',
+    ...deployConfigBase,
     host: '185.255.131.171',
     ssh_options: 'Port=1503',
     path: '/home/marco/work/pm2-apps/fishprovider',
@@ -165,3 +213,8 @@ module.exports = {
   apps,
   deploy,
 };
+
+// Examples:
+// DEPLOY_ENV=fish npm run pm2-deploy
+// DEPLOY_ENV=fish npm run pm2-deploy-build workers/cron
+// DEPLOY_ENV=fish npm run pm2-deploy-start cron
