759e4caafb39865935c2c8f90420ca61357cb162
Marco Dinh <maidh91@gmail.com>
2023-09-10 17:07:09 +1000

    fix clean

--- scripts/clean-frontend-deps.sh
+++ scripts/clean-frontend-deps.sh
@@ -2,8 +2,9 @@
 
 APP=$1
 
-cd ../packages && ls | grep -vE "utils|cross|core-new" | xargs rm -rf
+cd ../packages && ls | grep -vE "utils|cross" | xargs rm -rf
 
 cd ../packages-backend && rm -rf *
 cd ../apps-backend && rm -rf *
+
 cd ../apps-frontend && ls | grep -vE "$APP" | xargs rm -rf
