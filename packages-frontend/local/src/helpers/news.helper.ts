cec851dfbe8dcdaf56c30bc627a04e218e4bed62
Marco Dinh <maidh91@gmail.com>
2023-09-10 14:28:16 +1000

    local-ok

--- packages-frontend/local/src/helpers/news.helper.ts
+++
@@ -1,6 +0,0 @@
-import { GetNewsFilter } from '@fishprovider/core';
-
-export const buildNewsKeys = (filter: GetNewsFilter) => {
-  const keys = Object.entries(filter).map(([key, value]) => `${key}-${value}`);
-  return `news-${keys?.join('-')}`;
-};
--- packages-frontend/local/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './helpers';
-export * from './main';
-export * from './repositories';
