b49d93beab5dd849397f0b03565ec75d4427eb02
Marco Dinh <maidh91@gmail.com>
2023-09-12 23:24:27 +1000

    restart.sh

--- restart.sh
+++ restart.sh
@@ -4,5 +4,6 @@ APP=$1
 APP_TYPE=${2:-backend}
 DEPLOY_ENV=${3:-fish}
 
+DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-stop $APP
 DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-build apps-$APP_TYPE/$APP
 DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-start $APP
