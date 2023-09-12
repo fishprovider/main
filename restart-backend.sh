8ef6cc637d7992283880ee09a4746bf70c2e3dff
Marco Dinh <maidh91@gmail.com>
2023-09-12 23:23:58 +1000

    restart.sh

--- restart-backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DEPLOY_ENV=$1
-APP_BACKEND=$2
-
-DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-build apps-backend/$APP_BACKEND
-DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-start $APP_BACKEND
---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+APP=$1
+APP_TYPE=${2:-backend}
+DEPLOY_ENV=${3:-fish}
+
+DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-build apps-$APP_TYPE/$APP
+DEPLOY_ENV=$DEPLOY_ENV npm run pm2-deploy-start $APP
