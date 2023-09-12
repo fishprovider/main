4c935614a54e38557e5f37c7d1d1b14b24b8616f
Marco Dinh <maidh91@gmail.com>
2023-09-12 23:38:22 +1000

    restart-secondary.sh

---
+++ restart-secondary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+APP=$1
+APP_TYPE=${2:-backend}
+DEPLOY_ENV=fishSecondary
+
+DOPPLER_TOKEN=$(doppler configure get token --plain) pm2 deploy pm2.config.cjs $DEPLOY_ENV \
+exec "source ~/.zshrc; npm run pm2-stop $APP-secondary; npm run build -w apps-$APP_TYPE/$APP; npm run pm2-start $APP-secondary"
