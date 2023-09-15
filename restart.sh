4cde8dc7fca30e8bb8bff2c377c007e20a707274
Marco Dinh <maidh91@gmail.com>
2023-09-15 21:08:08 +1000

    pm2

--- restart.sh
+++ restart.sh
@@ -5,4 +5,4 @@ APP_TYPE=${2:-backend}
 DEPLOY_ENV=${3:-fish}
 
 DOPPLER_TOKEN=$(doppler configure get token --plain) pm2 deploy pm2.config.cjs $DEPLOY_ENV \
-exec "source ~/.zshrc; npm run pm2-stop $APP; npm run build -w apps-$APP_TYPE/$APP; npm run pm2-start $APP"
+exec "./pm2-preload.sh; npm run pm2-stop $APP; npm run build -w apps-$APP_TYPE/$APP; npm run pm2-start $APP"
