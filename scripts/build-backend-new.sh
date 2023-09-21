f59bc44ce698851944136e147bfa6b6d60f013d9
Marco Dinh <maidh91@gmail.com>
2023-09-22 06:52:06 +1000

    fix build

--- scripts/build-backend-new.sh
+++ scripts/build-backend-new.sh
@@ -12,6 +12,7 @@ function notif() {
   npm run build -w packages-backend/slack &
   npm run build -w packages-backend/discord &
   npm run build -w packages-backend/expo &
+  wait
   npm run build -w packages-backend/notif
 }
 
@@ -19,6 +20,7 @@ function dataAccess() {
   npm run build -w packages-backend/firebase &
   npm run build -w packages-backend/mongo &
   npm run build -w packages-backend/redis &
+  wait
   npm run build -w packages-backend/data-access
 }
 
@@ -26,6 +28,7 @@ function trade() {
   npm run build -w packages-backend/ctrader-api &
   npm run build -w packages-backend/metatrader-api &
   npm run build -w packages-backend/meta-api &
+  wait
   npm run build -w packages-backend/trade
 }
 
