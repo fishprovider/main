6f36cb5f5a42ae1e14d781fb542ab0127e8dad2b
Marco Dinh <maidh91@gmail.com>
2023-09-21 14:42:16 +1000

    fix-build

--- scripts/build-frontend-new.sh
+++ scripts/build-frontend-new.sh
@@ -9,9 +9,9 @@ fi
 cd ..
 
 
-npm run build -w packages-backend/fish-api &
-npm run build -w packages-backend/local &
-npm run build -w packages-backend/store &
+npm run build -w packages-frontend/fish-api &
+npm run build -w packages-frontend/local &
+npm run build -w packages-frontend/store &
 wait
 
 npm run build -w packages-frontend/data-fetch
