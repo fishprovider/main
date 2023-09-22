48a45055e863a2dd488892d530a534be15bd6575
Marco Dinh <maidh91@gmail.com>
2023-09-22 10:12:10 +1000

    res-men

---
+++ apps-frontend/web/layouts/BaseLayout/Header/MenuLeft/MenuItems.tsx
@@ -0,0 +1,78 @@
+import { useRouter } from 'next/router';
+
+import Link from '~components/base/Link';
+import { menuItems } from '~constants/menu';
+import Routes from '~libs/routes';
+import Avatar from '~ui/core/Avatar';
+import Button from '~ui/core/Button';
+import Group from '~ui/core/Group';
+import Icon from '~ui/core/Icon';
+import Menu from '~ui/core/Menu';
+import Text from '~ui/core/Text';
+
+interface Props {
+  numMenuItems?: number;
+  offset?: number;
+}
+
+function MenuItems({ numMenuItems = 0, offset = 0 }: Props) {
+  const router = useRouter();
+
+  const fullItems = menuItems.slice(offset, numMenuItems + offset);
+  const dropdownItems = menuItems.slice(numMenuItems + offset);
+
+  const renderFullItems = () => fullItems.map((item) => {
+    const isSelect = item.href === Routes.home
+      ? router.pathname === Routes.home
+      : router.pathname.startsWith(item.href);
+    return (
+      <Link key={item.key} href={item.href} variant="clean">
+        <Button
+          variant={isSelect ? 'light' : 'subtle'}
+          data-test-id={`navbar.${item.key}`}
+        >
+          {item.label}
+        </Button>
+      </Link>
+    );
+  });
+
+  const renderDropdownItems = () => (dropdownItems.length ? (
+    <Menu
+      items={dropdownItems.map((menuItem) => ({
+        key: menuItem.key,
+        content: (
+          <Link href={menuItem.href} variant="clean">
+            <span data-test-id={`navbar.${menuItem.key}`}>
+              <Text size="lg">{menuItem.label}</Text>
+            </span>
+          </Link>
+        ),
+        onClick: () => router.push(menuItem.href),
+      }))}
+    >
+      <span data-test-id="navbar.menu">
+        <Icon name="List" button size="large" />
+      </span>
+    </Menu>
+  ) : null);
+
+  return (
+    <Group p={5} spacing="xs">
+      <Link href={Routes.home} variant="clean">
+        <Avatar
+          src="/logo.png"
+          alt="logo"
+          size="md"
+          imageProps={{
+            loading: 'eager',
+          }}
+        />
+      </Link>
+      {renderFullItems()}
+      {renderDropdownItems()}
+    </Group>
+  );
+}
+
+export default MenuItems;
--- apps-frontend/web/layouts/BaseLayout/Header/MenuLeft/index.tsx
+++ apps-frontend/web/layouts/BaseLayout/Header/MenuLeft/index.tsx
@@ -1,10 +1,22 @@
 import useMaxWidth from '~ui/styles/useMaxWidth';
 
-import MenuFull from './MenuFull';
-import MenuMini from './MenuMini';
+import MenuItems from './MenuItems';
 
 function MenuLeft() {
-  return useMaxWidth('1000px') ? <MenuMini /> : <MenuFull />;
+  const isDropdownOnly = useMaxWidth('450px');
+  const isTiny = useMaxWidth('550px');
+  const isMini = useMaxWidth('650px');
+  const isSmall = useMaxWidth('750px');
+  const isMedium = useMaxWidth('850px');
+  const isLarge = useMaxWidth('1050px');
+
+  if (isDropdownOnly) return <MenuItems numMenuItems={0} offset={1} />;
+  if (isTiny) return <MenuItems numMenuItems={1} offset={1} />;
+  if (isMini) return <MenuItems numMenuItems={2} offset={1} />;
+  if (isSmall) return <MenuItems numMenuItems={3} offset={1} />;
+  if (isMedium) return <MenuItems numMenuItems={4} offset={1} />;
+  if (isLarge) return <MenuItems numMenuItems={5} />;
+  return <MenuItems numMenuItems={100} />;
 }
 
 export default MenuLeft;
