7bda529318ab9d80c0aa352f44206847084798f2
Marco Dinh <maidh91@gmail.com>
2023-09-28 14:49:52 +1000

    script

--- apps-frontend/web/deploy-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./git-config.sh
-
-git checkout dev; git pull origin dev; git merge master --no-verify
-
-git checkout dev; git push origin dev
-git checkout master; git push origin master
-
--- apps-frontend/web/deploy-legacy.sh
+++ apps-frontend/web/deploy-legacy.sh
@@ -4,7 +4,9 @@
 
 git checkout dev; git pull origin dev; git merge master --no-verify
 git checkout canary; git pull origin canary; git merge dev --no-verify
+git checkout release; git pull origin release; git merge canary --no-verify
 
+git checkout release; git push origin release
 git checkout canary; git push origin canary
 git checkout dev; git push origin dev
 git checkout master; git push origin master
