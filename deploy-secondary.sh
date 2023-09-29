50f72a4e58884428a13e8a55dcfd324faa600367
Marco Dinh <maidh91@gmail.com>
2023-09-29 10:37:47 +1000

    APP_SECONDARY

--- deploy-secondary.sh
+++ deploy-secondary.sh
@@ -5,6 +5,7 @@ APP_TYPE=$2 # backend or frontend
 DEPLOY_ENV=fishSecondary
 
 APP_DIR=apps-$APP_TYPE/$APP
+APP_SECONDARY=$APP-secondary
 
 # push tag (force)
 git tag secondary -f
@@ -18,4 +19,4 @@ pm2 deploy pm2.config.cjs $DEPLOY_ENV \
 exec "source ./pm2-preload.sh; npm i -w $APP_DIR"
 
 # stop, build, and start
-source ./restart.sh $APP $APP_TYPE $DEPLOY_ENV
+source ./restart.sh $APP_SECONDARY $APP_TYPE $DEPLOY_ENV
