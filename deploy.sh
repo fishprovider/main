8a447a692276e60adbbf224d33d43823b4807868
Marco Dinh <maidh91@gmail.com>
2023-09-29 10:58:45 +1000

    npm ci --prefer-offline --no-audit --no-progress --legacy-peer-deps

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ APP_DIR=apps-$APP_TYPE/$APP
 # pull
 pm2 deploy pm2.config.cjs $DEPLOY_ENV
 
-# npm
+# npm ci
 pm2 deploy pm2.config.cjs $DEPLOY_ENV \
-exec "source ./pm2-preload.sh; npm i -w $APP_DIR"
+exec "source ./pm2-preload.sh; npm ci --prefer-offline --no-audit --no-progress --legacy-peer-deps -w $APP_DIR"
 
 # stop, build, and start
 source ./restart.sh $APP $APP_TYPE $DEPLOY_ENV
