eefcc7a6ec5bb067c09514066fd02dabc51faf27
Marco Dinh <maidh91@gmail.com>
2023-10-10 06:53:58 +1100

    npm run pm2

--- restart-secondary.sh
+++ restart-secondary.sh
@@ -8,4 +8,4 @@ APP_DIR=apps-$APP_TYPE/$APP
 APP_NAME=$APP-secondary
 
 DOPPLER_TOKEN=$(doppler configure get token --plain) pm2 deploy pm2.config.cjs $DEPLOY_ENV \
-exec "source ./pm2-preload.sh; npm run pm2-stop $APP_NAME; npm run build -w $APP_DIR; npm run pm2-start $APP_NAME"
+exec "source ./pm2-preload.sh; pm2 stop pm2.config.cjs --only $APP_NAME; npm run build -w $APP_DIR; pm2 startOrReload pm2.config.cjs --only $APP_NAME"
--- restart.sh
+++ restart.sh
@@ -7,4 +7,4 @@ DEPLOY_ENV=$3
 APP_DIR=apps-$APP_TYPE/$APP
 
 DOPPLER_TOKEN=$(doppler configure get token --plain) pm2 deploy pm2.config.cjs $DEPLOY_ENV \
-exec "source ./pm2-preload.sh; npm run pm2-stop $APP; npm run build -w $APP_DIR; npm run pm2-start $APP"
+exec "source ./pm2-preload.sh; pm2 stop pm2.config.cjs --only $APP; npm run build -w $APP_DIR; pm2 startOrReload pm2.config.cjs --only $APP"
