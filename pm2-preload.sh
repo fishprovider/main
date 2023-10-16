371a5dbfff4edb4ffa0196bd4f170fd70cfa15f5
Marco Dinh <maidh91@gmail.com>
2023-10-16 11:57:29 +1100

    pm2rc.sh

--- pm2-preload.sh
+++ pm2-preload.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-source ~/.zshrc
+export HUSKY=0
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+
+source ~/pm2rc.sh || echo "No ~/pm2rc.sh found"
