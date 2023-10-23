6281dac83035e91b62d983c0808c3482009371b5
Marco Dinh <maidh91@gmail.com>
2023-10-23 19:39:36 +1100

    master

--- apps-frontend/web/deploy-legacy.sh
+++ apps-frontend/web/deploy-legacy.sh
@@ -5,11 +5,9 @@ REMOTE=vercel
 git config --global push.default current
 git config --global checkout.defaultRemote $REMOTE
 
-git checkout dev; git pull $REMOTE dev; git merge master --no-verify
-git checkout canary; git pull $REMOTE canary; git merge dev --no-verify
+git checkout canary; git pull $REMOTE canary; git merge master --no-verify
 git checkout legacy; git pull $REMOTE legacy; git merge canary --no-verify
 
 git checkout legacy; git push $REMOTE legacy
 git checkout canary; git push $REMOTE canary
-git checkout dev; git push $REMOTE dev
 git checkout master; git push $REMOTE master
