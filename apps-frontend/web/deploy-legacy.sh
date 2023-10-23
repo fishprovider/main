3728bef57d4014360e7eb6161423416639ba1047
Marco Dinh <maidh91@gmail.com>
2023-10-23 19:36:04 +1100

    vercel legacy

--- apps-frontend/web/deploy-legacy.sh
+++ apps-frontend/web/deploy-legacy.sh
@@ -7,9 +7,9 @@ git config --global checkout.defaultRemote $REMOTE
 
 git checkout dev; git pull $REMOTE dev; git merge master --no-verify
 git checkout canary; git pull $REMOTE canary; git merge dev --no-verify
-git checkout release; git pull $REMOTE release; git merge canary --no-verify
+git checkout legacy; git pull $REMOTE legacy; git merge canary --no-verify
 
-git checkout release; git push $REMOTE release
+git checkout legacy; git push $REMOTE legacy
 git checkout canary; git push $REMOTE canary
 git checkout dev; git push $REMOTE dev
 git checkout master; git push $REMOTE master
