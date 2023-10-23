3da72d40feaa1e68ac4d2c55c8957f196ef39173
Marco Dinh <maidh91@gmail.com>
2023-10-23 16:55:00 +1100

    REMOTE=vercel

--- apps-frontend/web/deploy-legacy.sh
+++ apps-frontend/web/deploy-legacy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+REMOTE=vercel
+
 git config --global push.default current
-git config --global checkout.defaultRemote origin
+git config --global checkout.defaultRemote $REMOTE
 
-git checkout dev; git pull origin dev; git merge master --no-verify
-git checkout canary; git pull origin canary; git merge dev --no-verify
-git checkout release; git pull origin release; git merge canary --no-verify
+git checkout dev; git pull $REMOTE dev; git merge master --no-verify
+git checkout canary; git pull $REMOTE canary; git merge dev --no-verify
+git checkout release; git pull $REMOTE release; git merge canary --no-verify
 
-git checkout release; git push origin release
-git checkout canary; git push origin canary
-git checkout dev; git push origin dev
-git checkout master; git push origin master
+git checkout release; git push $REMOTE release
+git checkout canary; git push $REMOTE canary
+git checkout dev; git push $REMOTE dev
+git checkout master; git push $REMOTE master
