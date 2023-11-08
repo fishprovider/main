7eda36fe5dc5affc66884a230796ddc4c33d53bb
Marco Dinh <maidh91@gmail.com>
2023-11-08 21:23:37 +1100

    FishApi

--- apps-frontend/phone/services/account/getAccount.service.ts
+++ apps-frontend/phone/services/account/getAccount.service.ts
@@ -1,17 +1,16 @@
 import { checkRepository } from '@fishprovider/core';
 import storeAccounts from '@fishprovider/cross/dist/stores/accounts';
-import { StoreFirstAccountRepository } from '@fishprovider/store-first';
+import { FishApiAccountRepository } from '@fishprovider/fish-api';
 import { Account } from '@fishprovider/utils/types/Account.model';
 
 export const getAccountService = async (filter: {
   accountId: string,
   getTradeInfo?: boolean,
 }) => {
-  const getAccountRepo = checkRepository(StoreFirstAccountRepository.getAccount);
+  const getAccountRepo = checkRepository(FishApiAccountRepository.getAccount);
   const { doc } = await getAccountRepo(filter);
 
   if (doc) {
-    // TODO: migrate to StoreFirstAccountRepository
     storeAccounts.mergeDoc(doc as Partial<Account>);
   }
 
--- apps-frontend/phone/services/account/getAccounts.service.ts
+++ apps-frontend/phone/services/account/getAccounts.service.ts
@@ -1,17 +1,16 @@
 import { AccountViewType, checkRepository } from '@fishprovider/core';
 import storeAccounts from '@fishprovider/cross/dist/stores/accounts';
-import { StoreFirstAccountRepository } from '@fishprovider/store-first';
+import { FishApiAccountRepository } from '@fishprovider/fish-api';
 import { Account } from '@fishprovider/utils/types/Account.model';
 
 export const getAccountsService = async (filter: {
   accountViewType?: AccountViewType,
   email?: string,
 }) => {
-  const getAccountsRepo = checkRepository(StoreFirstAccountRepository.getAccounts);
+  const getAccountsRepo = checkRepository(FishApiAccountRepository.getAccounts);
   const { docs: accounts } = await getAccountsRepo(filter);
 
   if (accounts) {
-    // TODO: migrate to StoreFirstAccountRepository
     storeAccounts.mergeDocs(accounts as Partial<Account>[]);
   }
 
