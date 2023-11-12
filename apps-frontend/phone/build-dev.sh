a790bde1d607cfd9955fee46665953fa0894f399
Marco Dinh <maidh91@gmail.com>
2023-11-13 10:02:34 +1100

    build-dev

--- apps-frontend/phone/build-dev.sh
+++ apps-frontend/phone/build-dev.sh
@@ -2,7 +2,9 @@
 
 command -v rbenv > /dev/null && eval "$(rbenv init - zsh)"
 
+rm -rf ~/work/fish/builds/*
+
 EAS_LOCAL_BUILD_SKIP_CLEANUP=1 \
 EAS_LOCAL_BUILD_WORKINGDIR=~/work/fish/builds \
 EAS_LOCAL_BUILD_ARTIFACTS_DIR=~/work/fish/artifacts \
-npx eas build --non-interactive --no-wait --profile development --local -p ios
+BUILD_PROFILE=development npm run build-local-ios
