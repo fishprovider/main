8fb45cee5d3679d2469afd272b4834a9c5a41aea
Marco Dinh <maidh91@gmail.com>
2023-11-15 09:41:28 +1100

    green-series

--- apps-frontend/web/views/Strategies/StrategyCatalog/index.tsx
+++ apps-frontend/web/views/Strategies/StrategyCatalog/index.tsx
@@ -125,9 +125,9 @@ function Catalog() {
         </Title>
         <ContentSection>
           <Title size="h4" ta="center">
-            These strategies focus on eco-friendly investments.
-            Profits are used to support environmentally-friendly initiatives,
-            balancing good returns with sustainability for a greener future.
+            These strategies focus on eco-friendly investments where profits
+            are used to support environmentally-friendly initiatives,
+            balancing good returns with sustainability for a greener future
           </Title>
         </ContentSection>
         <ProviderCards
