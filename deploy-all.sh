ac20ccc3d1b1b769b0885da372dd18ed98f64afe
Marco Dinh <maidh91@gmail.com>
2023-11-17 07:24:38 +1100

    deploy-all

---
+++ deploy-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./deploy.sh bot; ./deploy.sh copy; ./deploy.sh pay
+./deploy.sh cron; ./deploy.sh gate; ./deploy.sh mon
+./deploy.sh head-ctrader; ./deploy.sh head-meta
+./deploy.sh spot-ctrader; ./deploy.sh spot-meta
+./deploy.sh spot-ctrader-poll; ./deploy.sh spot-meta-poll
+./deploy.sh back; ./deploy.sh back-secondary
+./deploy.sh web; ./deploy.sh web-secondary
