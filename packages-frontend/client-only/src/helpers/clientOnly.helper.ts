91650821e0507bd7ff15e526754eab43a757ffe3
Marco Dinh <maidh91@gmail.com>
2023-11-25 23:28:53 +1100

    clientOnly

--- packages-frontend/client-only/src/helpers/clientOnly.helper.ts
+++ packages-frontend/client-only/src/helpers/clientOnly.helper.ts
@@ -3,27 +3,6 @@ interface Base {
   docs?: any;
 }
 
-export const getClientOnly = async <T extends Base>(params: {
-  getStore?: () => Promise<T>,
-  setStore?: (data?: T) => Promise<T>,
-  getLocal?: () => Promise<T>,
-}) => {
-  const {
-    getStore, setStore, getLocal,
-  } = params;
-
-  let data: T | undefined = await getStore?.();
-
-  if (!data?.doc && !data?.docs) {
-    data = await getLocal?.();
-    setStore?.(data); // non-blocking
-  } else {
-    getLocal?.().then(setStore); // non-blocking
-  }
-
-  return data;
-};
-
 export const updateClientOnly = async <T extends Base>(params: {
   updateLocal?: (data?: T) => Promise<T>,
   updateStore?: (data?: T) => Promise<T>,
