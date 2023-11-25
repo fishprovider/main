65e86af4253509fb96850e84387952f2c0811610
Marco Dinh <maidh91@gmail.com>
2023-11-25 21:21:46 +1100

    postbuild

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+ls -1 ../../packages | while read PROJECT; do
+  npm run postbuild -w ../../packages/$PROJECT
+done
+
 grep node_modules -rq --include=\*.js dist || exit 0 && echo 'node_modules found in dist' && exit 1
