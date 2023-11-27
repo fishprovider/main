999f142d5ebbae6a97c91369ad0ff2644eef02e6
Marco Dinh <maidh91@gmail.com>
2023-11-28 09:55:09 +1100

    build-dev

--- apps-frontend/phone/build-dev.sh
+++ apps-frontend/phone/build-dev.sh
@@ -5,4 +5,5 @@ rm -rf ~/work/fish/builds/*
 EAS_LOCAL_BUILD_SKIP_CLEANUP=1 \
 EAS_LOCAL_BUILD_WORKINGDIR=~/work/fish/builds \
 EAS_LOCAL_BUILD_ARTIFACTS_DIR=~/work/fish/artifacts \
-BUILD_PROFILE=development npm run build-local-ios
+BUILD_PROFILE=development \
+npm run build-local-android
