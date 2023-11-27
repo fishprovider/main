e92921cc54bd1318e4524c36cf1ced24e1b25d0f
Marco Dinh <maidh91@gmail.com>
2023-11-28 09:56:01 +1100

    skip-doctor debug

--- apps-frontend/phone/build-debug.sh
+++ apps-frontend/phone/build-debug.sh
@@ -5,5 +5,5 @@ rm -rf ~/work/fish/builds/*
 EAS_LOCAL_BUILD_SKIP_CLEANUP=1 \
 EAS_LOCAL_BUILD_WORKINGDIR=~/work/fish/builds \
 EAS_LOCAL_BUILD_ARTIFACTS_DIR=~/work/fish/artifacts \
-BUILD_PROFILE=development \
+BUILD_PROFILE=production \
 npm run build-local-android
