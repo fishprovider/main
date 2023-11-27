72bdb949887792cff34d390ff21309f13b44ad0b
Marco Dinh <maidh91@gmail.com>
2023-11-28 10:01:53 +1100

    skip-doctor debug

--- apps-frontend/phone/build-debug.sh
+++ apps-frontend/phone/build-debug.sh
@@ -2,6 +2,8 @@
 
 rm -rf ~/work/fish/builds/*
 
+export CI=false
+
 EAS_LOCAL_BUILD_SKIP_CLEANUP=1 \
 EAS_LOCAL_BUILD_WORKINGDIR=~/work/fish/builds \
 EAS_LOCAL_BUILD_ARTIFACTS_DIR=~/work/fish/artifacts \
