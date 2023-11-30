0309fb4f01f7062c350e277f67af2a0b59e0719b
Marco Dinh <maidh91@gmail.com>
2023-11-30 21:12:17 +1100

    fix-refresh

--- apps-backend/back/apiV3/user/refreshUserRoles.ts
+++ apps-backend/back/apiV3/user/refreshUserRoles.ts
@@ -1,6 +1,7 @@
-import { CacheFirstAccountRepository, CacheFirstUserRepository } from '@fishprovider/cache-first';
+import { CacheFirstUserRepository } from '@fishprovider/cache-first';
 import { User } from '@fishprovider/core';
 import { refreshUserRolesService } from '@fishprovider/core-backend';
+import { MongoAccountRepository } from '@fishprovider/mongo';
 import { z } from 'zod';
 
 import { ApiHandler } from '~types/ApiHandler.model';
@@ -15,7 +16,7 @@ const refreshUserRoles: ApiHandler<Partial<User>> = async (data, userSession) =>
       email: userSession?.email,
     },
     repositories: {
-      account: CacheFirstAccountRepository,
+      account: MongoAccountRepository,
       user: CacheFirstUserRepository,
     },
     context: { userSession },
