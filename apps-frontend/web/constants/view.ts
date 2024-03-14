e341d1138fe207d7a8d9e2dd3630de814a44e632
Marco Dinh <maidh91@gmail.com>
2024-03-14 14:56:10 +1100

    notion-pages

--- apps-frontend/web/constants/view.ts
+++ apps-frontend/web/constants/view.ts
@@ -1,12 +1,12 @@
-const notionPages = {
+export const notionPages = {
   faq: {
     rootId: 'b116a2e02cdc42c4b3896a1e00346d91',
   },
   blog: {
-    rootId: 'b1e407e736224167b61fc1c5dcc314d2',
+    rootId: 'df00ee4c71aa4ef9b6b3e2d17536b46d',
   },
   news: {
-    rootId: 'a55d5ec4332f4b088edf56330174ddb4',
+    rootId: '34f5ce8ed50c47beb15417c9b6e4e0a1',
   },
   hotNews: {
     rootId: '4f9c972dfd054df8bacdc20e82cb4057',
@@ -14,8 +14,4 @@ const notionPages = {
   reports: {
     rootId: 'ebc7f43e6b5342729502e01ac57fc4f1',
   },
-};
-
-export {
-  notionPages,
 };
