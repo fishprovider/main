98b5215e57271a81da3ca7ae2ffe2cd6e140960c
Marco Dinh <maidh91@gmail.com>
2024-07-01 09:44:32 +1000

    jul

--- scripts/db/clean-db.js
+++ scripts/db/clean-db.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 
 db.getCollection('orders').deleteMany({
-  createdAt: { $lt: new Date('2024 Feb 1') },
+  createdAt: { $lt: new Date('2024 May 1') },
 });
 db.getCollection('signals').deleteMany({
-  createdAt: { $lt: new Date('2024 Feb 1') },
+  createdAt: { $lt: new Date('2024 May 1') },
 });
--- scripts/db/update-target.js
+++ scripts/db/update-target.js
@@ -16,7 +16,7 @@ db.accounts.find({
   const offset = 0.1;
   const monthProfit = +doc.monthProfit + offset;
   const monthTargetLock = Math.round((doc.balance + (doc.balance * monthProfit) / 100) * 100) / 100;
-  const copyVolumeRatioFixed = Math.round((doc.balance / 1000) * 100) / 100;
+  const copyVolumeRatioFixed = Math.max(1, Math.round((doc.balance / 1000) * 100) / 100);
 
   db.accounts.updateOne({
     _id: doc._id,
