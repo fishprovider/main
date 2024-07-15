b8c671a3b2523fcfc8668a4191c132c02a91de57
Marco Dinh <maidh91@gmail.com>
2024-07-15 17:27:35 +0700

    disable-some-apps

--- deploy-reload-all.sh
+++ deploy-reload-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./deploy-reload.sh bot; ./deploy-reload.sh copy; ./deploy-reload.sh pay
-./deploy-reload.sh cron; ./deploy-reload.sh gate; ./deploy-reload.sh mon
+./deploy-reload.sh bot; ./deploy-reload.sh copy; ./deploy-reload.sh cron
+# ./deploy-reload.sh pay; ./deploy-reload.sh gate; ./deploy-reload.sh mon
 ./deploy-reload.sh head-ctrader; ./deploy-reload.sh head-meta
 ./deploy-reload.sh spot-ctrader; ./deploy-reload.sh spot-meta
 ./deploy-reload.sh spot-ctrader-poll; ./deploy-reload.sh spot-meta-poll
